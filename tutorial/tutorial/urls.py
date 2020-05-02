a1539950584110690757e7697e4cda47f6ac81cf
Stefan Krone <dev@st-page.de>
2020-05-02 23:26:22 +0200

    Tutorial, Tutorial 4: Authentication & Permissions

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import include
 from rest_framework import routers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('snippets.urls')),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls')),
 ]
